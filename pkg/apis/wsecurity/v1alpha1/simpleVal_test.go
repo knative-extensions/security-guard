f16b861cd4d2484d10954d715453c5614f9463b6
David Hadas <david.hadas@gmail.com>
2022-08-17 19:49:08 +0300

    reducted lorem ipsum

--- pkg/apis/wsecurity/v1alpha1/simpleVal_test.go
+++ pkg/apis/wsecurity/v1alpha1/simpleVal_test.go
@@ -9,7 +9,7 @@ const (
 	hebrew2    = "A23/*מכבימכבי*/"
 	chineese   = "A23/*世界世界*/"
 	loremIpsum = `What is Lorem Ipsum?
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was xxx in the 1960s with the release of xxx sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like xxx PageMaker including versions of Lorem Ipsum.
+				Lorem Ipsum is simply xxxxx text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard xxxxx text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was xxx in the 1960s with the release of xxx sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like xxx PageMaker including versions of Lorem Ipsum.
 
 				Why do we use it?
 				`
