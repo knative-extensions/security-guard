10bf1206f91e0aafda1df25734ed35dd99884e1d
David Hadas <david.hadas@gmail.com>
2022-09-07 19:25:52 +0300

    Code Generation + Updated Readme

---
+++ pkg/apis/wsecurity/v1alpha1/guardianSpec.go
@@ -0,0 +1,31 @@
+package v1alpha1
+
+type Ctrl struct {
+	Alert bool `json:"alert"` // If true, use SessionDataConfig to identify alerts
+	Block bool `json:"block"` // If true, block on alert.
+	Learn bool `json:"learn"` // If true, and no alert identified, report piles
+	Force bool `json:"force"` // If true, learning is done even when alert identified, report piles
+	Auto  bool `json:"auto"`  // If true, use learned SessionDataConfig rather than configured SessionDataConfig
+}
+
+type GuardianSpec struct {
+	Configured *SessionDataConfig `json:"configured"`        // configrued criteria
+	Learned    *SessionDataConfig `json:"learned,omitempty"` // Learned citeria
+	Control    *Ctrl              `json:"control"`           // Control
+}
+
+func (g *GuardianSpec) AutoActivate() {
+	if g.Control == nil {
+		g.Control = new(Ctrl)
+	}
+	g.Control.Auto = true
+	g.Control.Learn = true
+	g.Control.Force = true
+	g.Control.Alert = true
+}
+
+func (g *GuardianSpec) Reconcile() {
+	if g.Control == nil {
+		g.Control = new(Ctrl)
+	}
+}
---
+++ scripts/update-codegen.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash 
+
+# Copyright 2017 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+
+# echo "Generating clientset for ${GROUPS_WITH_VERSIONS} at ${OUTPUT_PKG}/${CLIENTSET_PKG_NAME:-clientset}"
+GOBIN="$(go env GOBIN)"
+gobin="${GOBIN:-$(go env GOPATH)/bin}"
+gosrc="${GOBIN:-$(go env GOPATH)/src}"
+projfullpath="$(cd ${SCRIPT_ROOT}; pwd)"
+boilerplate="${projfullpath}/scripts/boilerplate.go.txt"
+proj="github.com/knative-sandbox/security-guard"
+pkgapis="${proj}/pkg/apis/wsecurity/v1"
+outpack="${proj}/pkg/generated/clientset" 
+
+echo "pkgapis ${pkgapis}"
+echo "outpack ${outpack}"
+
+echo "Generating deepcopy funcs"
+"${gobin}/deepcopy-gen" \
+  --input-dirs $pkgapis \
+  --go-header-file  "${boilerplate}" \
+  -O zz_generated.deepcopy "$@"
+
+echo "Generating clientset"
+"${gobin}/client-gen" \
+  --input-base "" \
+  --input $pkgapis \
+  --output-base "$gosrc" \
+  --output-package "$outpack" \
+  --go-header-file  "${boilerplate}" \
+  --clientset-name "guardians" \
