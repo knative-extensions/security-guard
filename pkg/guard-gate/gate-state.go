6a6310633b488c2449cf02221975580d262acda0
David Hadas <david.hadas@gmail.com>
2022-09-21 23:48:13 +0300

    gateClient+gateState

---
+++ pkg/guard-gate/gate-state.go
@@ -0,0 +1,192 @@
+/*
+Copyright 2022 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package guardgate
+
+import (
+	"context"
+	"fmt"
+
+	spec "knative.dev/security-guard/pkg/apis/guard/v1alpha1"
+	utils "knative.dev/security-guard/pkg/guard-utils"
+)
+
+type gateState struct {
+	cancelFunc context.CancelFunc      // cancel the entire reverse proxy
+	ctrl       *spec.Ctrl              // gate Ctrl
+	criteria   *spec.SessionDataConfig // gate Criteria
+	stat       utils.Stat              // gate stats
+	alert      string                  // gate alert
+	monitorPod bool                    // should gate profile the pod?
+	pod        spec.PodProfile         // pod profile
+	srv        *gateClient             // maintainer of the pile, include client to the guard-service & kubeApi
+}
+
+func (gs *gateState) init(cancelFunc context.CancelFunc, monitorPod bool, guardServiceUrl string, sid string, ns string, useCm bool) {
+	gs.stat.Init()
+	gs.monitorPod = monitorPod
+	gs.cancelFunc = cancelFunc
+	gs.srv = new(gateClient)
+	gs.srv.init(guardServiceUrl, sid, ns, useCm)
+}
+
+// loadConfig is called periodically to load updated configuration from a Guardian
+func (gs *gateState) loadConfig() {
+	// loadGuardian never returns nil!
+	g := gs.srv.loadGuardian()
+
+	if gs.ctrl = g.Control; gs.ctrl == nil {
+		gs.ctrl = new(spec.Ctrl)
+	}
+
+	if gs.ctrl.Auto {
+		gs.criteria = g.Learned
+	} else {
+		gs.criteria = g.Configured
+	}
+	if gs.criteria == nil {
+		gs.criteria = new(spec.SessionDataConfig)
+	}
+}
+
+// flushPile is called periodically to send the pile to the guard-service
+func (gs *gateState) flushPile() {
+	gs.srv.reportPile()
+}
+
+// addProfile is called every time we have a new profile ready to be added to a pile
+func (gs *gateState) addProfile(profile *spec.SessionDataProfile) {
+	gs.srv.addToPile(profile)
+}
+
+// Methods to profile and decide base don pod data.
+// Enables the POD profile to be copied to the sessio  profile for reporting to pile.
+
+// profileAndDecidePod is called periodically to profile the pod and decide if to raise an alert
+func (gs *gateState) profileAndDecidePod() {
+	if !gs.monitorPod {
+		return
+	}
+	//First profile
+	gs.pod.Profile()
+
+	// Now decide
+	// Current behaviour is latching the alert forever
+	// This makes sense from security standpoint as the pod is now considered contaminated
+	// If we are blocking, this means we will simply keep blocking all requests forever
+	// Therefore we terminate the reverse proxy
+	// Future - add more controls to decide what to do in this situation
+	if gs.criteria.Active {
+		decision := gs.criteria.Pod.Decide(&gs.pod)
+		if decision != "" {
+			gs.addStat("PodAlert")
+			gs.alert = fmt.Sprintf("Pod: %s", decision)
+			logAlert(gs.alert)
+			// terminate the reverse proxy
+			gs.cancelFunc()
+		}
+	}
+}
+
+// if pod is monitored, copy its profile to the session profile
+func (gs *gateState) copyPodProfile(pp *spec.PodProfile) {
+	if !gs.monitorPod {
+		return
+	}
+	gs.pod.DeepCopyInto(pp)
+}
+
+// The following decide...() methods allow sessions to provide parts of the profile as they are being formed
+// the profiles are being decided and alerts is set accordingly.
+
+// returns the alert text if needed
+func (gs *gateState) decideReq(rp *spec.ReqProfile) string {
+	if gs.criteria.Active {
+		if decision := gs.criteria.Req.Decide(rp); decision != "" {
+			gs.addStat("ReqAlert")
+			return fmt.Sprintf("HttpRequest: %s", decision)
+		}
+	}
+	return ""
+}
+
+// returns the alert text if needed
+func (gs *gateState) decideResp(rp *spec.RespProfile) string {
+	if gs.criteria.Active {
+		if decision := gs.criteria.Resp.Decide(rp); decision != "" {
+			gs.addStat("RespAlert")
+			return fmt.Sprintf("HttpResponse: %s", decision)
+		}
+	}
+	return ""
+}
+
+// returns the alert text if needed
+func (gs *gateState) decideReqBody(bp *spec.BodyProfile) string {
+	if gs.criteria.Active {
+		if decision := gs.criteria.ReqBody.Decide(bp); decision != "" {
+			gs.addStat("ReqBodyAlert")
+			return fmt.Sprintf("HttpRequestBody: %s", decision)
+		}
+	}
+	return ""
+}
+
+// returns the alert text if needed
+func (gs *gateState) decideRespBody(bp *spec.BodyProfile) string {
+	if gs.criteria.Active {
+		if decision := gs.criteria.RespBody.Decide(bp); decision != "" {
+			gs.addStat("RespBodyAlert")
+			return fmt.Sprintf("HttpResponseBody: %s", decision)
+		}
+	}
+	return ""
+}
+
+// returns the alert text if needed
+func (gs *gateState) decideEnvelop(ep *spec.EnvelopProfile) string {
+	if gs.criteria.Active {
+		if decision := gs.criteria.Envelop.Decide(ep); decision != "" {
+			gs.addStat("EnvelopAlert")
+			return fmt.Sprintf("Envelop: %s", decision)
+		}
+	}
+	return ""
+}
+
+// generic methods:
+
+// advance statistics counter
+func (gs *gateState) addStat(key string) {
+	gs.stat.Add(key)
+}
+
+// are we blocking request on alerts?
+func (gs *gateState) shouldBlock() bool {
+	return gs.ctrl.Block
+}
+
+// do we have a gate level alert?
+func (gs *gateState) hasAlert() bool {
+	return gs.alert != ""
+}
+
+// should we be learning?
+func (gs *gateState) shouldLearn(sessionAlert bool) bool {
+	// dio we have an alert?
+	alert := (gs.alert != "") || !sessionAlert
+	return gs.ctrl.Learn && (!alert || gs.ctrl.Force)
+}
---
+++ pkg/guard-gate/gate-state_test.go
@@ -0,0 +1,157 @@
+/*
+Copyright 2022 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package guardgate
+
+import (
+	"net"
+	"net/http"
+	"reflect"
+	"testing"
+	"time"
+
+	spec "knative.dev/security-guard/pkg/apis/guard/v1alpha1"
+)
+
+var gateCanceled int
+
+func fakeGateCancel() {
+	gateCanceled++
+}
+
+func fakeGateState() *gateState {
+	gs := new(gateState)
+	gs.cancelFunc = fakeGateCancel
+	gs.stat.Init()
+	srv, _ := fakeClient(http.StatusOK, "Problem in request")
+	gs.srv = srv
+	return gs
+}
+
+func Test_gateState_loadConfig(t *testing.T) {
+	t.Run("simple", func(t *testing.T) {
+		gs := fakeGateState()
+
+		gs.loadConfig()
+		if gs.criteria == nil || gs.ctrl == nil {
+			t.Error("nil after load")
+		}
+		gs.profileAndDecidePod()
+		gs.monitorPod = true
+		gs.criteria.Active = true
+		gateCanceled = 0
+		gs.profileAndDecidePod()
+		if gateCanceled != 0 {
+			t.Error("expected no cancel")
+		}
+		var pp spec.PodProfile
+		gs.monitorPod = false
+		gs.copyPodProfile(&pp)
+		gs.monitorPod = true
+		gs.copyPodProfile(&pp)
+		if !reflect.DeepEqual(&gs.pod, &pp) {
+			t.Errorf("expected %v to be equal to %v", pp, gs.pod)
+		}
+		gs.addStat("XX")
+		gs.addStat("XX")
+		if ret := gs.stat.Log(); ret != "map[XX:2]" {
+			t.Errorf("expected stat.log to be %s rceived %s", "map[XX:2]", ret)
+		}
+		if gs.shouldBlock() != false {
+			t.Error("expected false in shouldBlock")
+		}
+		if gs.hasAlert() != false {
+			t.Error("expected false in hasAlert")
+		}
+		if gs.shouldLearn(true) != false {
+			t.Error("expected false in shouldLearn")
+		}
+
+		// envelop
+		ep := new(spec.EnvelopProfile)
+		now := time.Now()
+		ep.Profile(now, now, now)
+		if ret := gs.decideEnvelop(ep); ret != "" {
+			t.Error("expected no alert")
+		}
+		ep.Profile(time.Unix(1, 1), time.Unix(3, 3), time.Unix(5, 5))
+		gs.decideEnvelop(ep)
+		if ret := gs.decideEnvelop(ep); ret == "" {
+			t.Error("expected alert")
+		}
+
+		// req
+		req := new(spec.ReqProfile)
+		r, _ := http.NewRequest("Get", "", nil)
+		cip := net.ParseIP("1.2.3.4")
+		req.Profile(r, cip)
+		gs.criteria.Active = false
+		if ret := gs.decideReq(req); ret != "" {
+			t.Error("expected no alert")
+		}
+		gs.criteria.Active = true
+		if ret := gs.decideReq(req); ret == "" {
+			t.Error("expected alert")
+		}
+
+		// resp
+		resp := new(spec.RespProfile)
+		rs := &http.Response{ContentLength: 20}
+
+		resp.Profile(rs)
+		gs.criteria.Active = false
+		if ret := gs.decideResp(resp); ret != "" {
+			t.Error("expected no alert")
+		}
+		gs.criteria.Active = true
+		if ret := gs.decideResp(resp); ret == "" {
+			t.Error("expected alert")
+		}
+
+		// reqBody
+		body := new(spec.BodyProfile)
+		body.ProfileUnstructured("x")
+		gs.criteria.Active = false
+		if ret := gs.decideReqBody(body); ret != "" {
+			t.Error("expected no alert")
+		}
+		if ret := gs.decideRespBody(body); ret != "" {
+			t.Error("expected no alert")
+		}
+		gs.criteria.Active = true
+		if ret := gs.decideReqBody(body); ret == "" {
+			t.Error("expected alert")
+		}
+		if ret := gs.decideRespBody(body); ret == "" {
+			t.Error("expected alert")
+		}
+
+		gs.flushPile()
+		if gs.srv.pile.Count != 0 {
+			t.Error("expected pile too have 1")
+		}
+		profile := new(spec.SessionDataProfile)
+		gs.addProfile(profile)
+		if gs.srv.pile.Count != 1 {
+			t.Error("expected pile too have 1")
+		}
+		gs.flushPile()
+		if gs.srv.pile.Count != 0 {
+			t.Error("expected pile too have 1")
+		}
+	})
+
+}
