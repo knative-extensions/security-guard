a649cbff3bad323ab0e154a04597f565b9c5a07f
David Hadas <david.hadas@gmail.com>
2022-11-08 16:41:31 +0200

    Update cmd/guard-rproxy/main.go

--- cmd/guard-rproxy/main.go
+++ cmd/guard-rproxy/main.go
@@ -96,7 +96,6 @@ func preMain() (guardGate *GuardGate, mux *http.ServeMux, target string, plugCon
 	utils.CreateLogger(env.LogLevel)
 
 	if env.GuardUrl == "" {
-		// use default
 		env.GuardUrl = "http://guard-service.knative-serving"
 	} else {
 		plugConfig["guard-url"] = env.GuardUrl
