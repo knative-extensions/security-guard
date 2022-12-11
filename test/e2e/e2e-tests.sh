e32d96a44ada2efa2af29966874161c90ed5cb00
David Hadas <david.hadas@gmail.com>
2022-12-11 16:49:48 +0200

    e2e

--- test/e2e/e2e-tests.sh
+++ test/e2e/e2e-tests.sh
@@ -6,6 +6,19 @@ kubectl logs deployment/httptest-00001-deployment queue-proxy
 response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"`
 responseEnd="${response#*Alert}"
 alert=${responseEnd%%\"*}
+
+echo "Alert Value: $alert"
+if [ "$alert" != "!" ]; then
+   exit 1
+fi
+
+curl "$URL?a=2"
+kubectl logs deployment/httptest-00001-deployment queue-proxy
+response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"`
+responseEnd="${response#*Alert}"
+alert=${responseEnd%%\"*}
+
+echo "Alert Value: $alert"
 if [ "$alert" != "!" ]; then
    exit 1
 fi
