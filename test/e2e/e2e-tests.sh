35afb4c6756aca38144e32dccdf70023641482d0
David Hadas <david.hadas@gmail.com>
2022-12-11 15:59:38 +0200

    e2e

--- test/e2e/e2e-tests.sh
+++ test/e2e/e2e-tests.sh
@@ -3,4 +3,9 @@ URL=$1
 echo "connecting to $URL"
 curl $URL
 kubectl logs deployment/httptest-00001-deployment queue-proxy
-kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"
+response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"`
+responseEnd="${response#*Alert}"
+alert=${responseEnd%%\"*}
+if [ "$alert" != "!" ]; then
+   return 1
+fi
