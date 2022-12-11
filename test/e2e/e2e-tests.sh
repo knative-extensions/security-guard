7d322787ce030ebea22fe41e0cc60024c673b022
David Hadas <david.hadas@gmail.com>
2022-12-11 17:18:08 +0200

    e2e

--- test/e2e/e2e-tests.sh
+++ test/e2e/e2e-tests.sh
@@ -3,7 +3,7 @@ URL=$1
 echo "connecting to $URL"
 curl $URL
 kubectl logs deployment/httptest-00001-deployment queue-proxy
-response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"`
+response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"|tail -1`
 responseEnd="${response#*Alert}"
 alert=${responseEnd%%\"*}
 
@@ -14,11 +14,26 @@ fi
 
 curl "$URL?a=2"
 kubectl logs deployment/httptest-00001-deployment queue-proxy
-response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"`
-responseEnd="${response#*Alert}"
+response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep "ALERT!"|tail -1`
+responseEnd="${response#*ALERT}"
 alert=${responseEnd%%\"*}
 
 echo "Alert Value: $alert"
-if [ "$alert" != "!" ]; then
+if [ "$alert" != "! HttpRequest -> [QueryString:[KeyVal:[Key a is not known,],],]" ]; then
    exit 1
 fi
+
+
+curl "$URL -H \"a:2\""
+kubectl logs deployment/httptest-00001-deployment queue-proxy
+response=`kubectl logs deployment/httptest-00001-deployment queue-proxy|grep "ALERT!"|tail -1`
+responseEnd="${response#*ALERT}"
+alert=${responseEnd%%\"*}
+
+echo "Alert Value: $alert"
+if [ "$alert" != "! HttpRequest -> [Header:[KeyVal:[Key a is not known,],],]" ]; then
+   exit 1
+fi
+
+echo "Done!"
+exit 0
