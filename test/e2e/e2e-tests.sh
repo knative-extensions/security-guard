f83657f923aab05fcab42629b8976e7512c52073
David Hadas <david.hadas@gmail.com>
2022-12-11 08:19:30 +0200

    e2e

--- test/e2e/e2e-tests.sh
+++ test/e2e/e2e-tests.sh
@@ -2,4 +2,5 @@
 URL=$1
 echo "connecting to $URL"
 curl $URL
+kubectl logs deployment/httptest-00001-deployment queue-proxy
 kubectl logs deployment/httptest-00001-deployment queue-proxy|grep -i "alert"
