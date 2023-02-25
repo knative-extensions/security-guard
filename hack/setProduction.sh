9c5b19db4d2fb727fe0899db0a1f085a7366f1bb
David Hadas <david.hadas@gmail.com>
2023-02-25 19:46:55 +0200

    create  serving-certs-ctrl-ca-public in setProduction

--- hack/setProduction.sh
+++ hack/setProduction.sh
@@ -25,6 +25,13 @@ ROOTCA="$(mktemp)"
 FILENAME=`basename $ROOTCA`
 kubectl get secret -n knative-serving knative-serving-certs -o json| jq -r '.data."ca-cert.pem"' | base64 -d >  $ROOTCA
 
+echo "(Re)Create secrets serving-certs-ctrl-ca-public in namespaces: default, knative-serving"
+kubectl delete secret serving-certs-ctrl-ca-public -n knative-serving
+kubectl delete secret serving-certs-ctrl-ca-public
+kubectl create secret generic serving-certs-ctrl-ca-public --from-file=ca.crt=$ROOTCA -n knative-serving
+kubectl create secret generic serving-certs-ctrl-ca-public --from-file=ca.crt=$ROOTCA -n default
+
+
 echo "Get the certificate in a configmap friendly form"
 CERT=`kubectl create cm config-deployment --from-file $ROOTCA -o json --dry-run=client |jq .data.\"$FILENAME\"`
 
